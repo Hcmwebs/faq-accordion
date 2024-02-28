315e42e4a45846b4edb9c61a25989c89ec35d692
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-28 23:02:48 +1300

    removed the transition

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import { Item, Title } from './index';
 
 const Card = () => {
 	return (
-		<div className='grid grid-col-1 place-item-center gap-y-5 p-6 bg-white w-full max-w-[327px] lg:max-w-[600px] rounded-lg shadow-xl hover:shadow-2xl absolute top-[90px] lg:top-[150px] left-[50%] -translate-x-1/2 transition ease-in-out duration-500'>
+		<div className='grid grid-col-1 place-item-center gap-y-5 p-6 bg-white w-full max-w-[327px] lg:max-w-[600px] rounded-lg shadow-xl hover:shadow-2xl absolute top-[90px] lg:top-[150px] left-[50%] -translate-x-1/2'>
 			<Title text='FAQs' />
 			<Item />
 		</div>

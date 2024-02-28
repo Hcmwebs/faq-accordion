a79d3793313783dd19d33cd9c3b677ec8e79be6d
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-28 21:49:37 +1300

    updated the content of the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,10 @@
-import { useState } from 'react';
-import { Title } from './index';
+import { Item, Title } from './index';
 
 const Card = () => {
-	const [show, setShow] = useState(false);
-
-	const toggleShow = () => {
-		setShow(!show);
-	};
 	return (
-		<div className='grid grid-col-1 place-item-center gap-y-6 p-6 bg-white w-full max-w-[327px] lg:max-w-[600px] rounded-lg shadow-xl hover:shadow-2xl absolute top-[90px] lg:top-[150px] left-[50%] -translate-x-1/2'>
+		<div className='grid grid-col-1 place-item-center gap-y-5 p-6 bg-white w-full max-w-[327px] lg:max-w-[600px] rounded-lg shadow-xl hover:shadow-2xl absolute top-[90px] lg:top-[150px] left-[50%] -translate-x-1/2 transition ease-in-out duration-500'>
 			<Title text='FAQs' />
-
-			<div className='grid grid-col-1 place-items-center gap-y-4 px-6 text-justify'>
-				<header className='flex gap-x-8'>
-					<h2>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi
-						facere ab nobis placeat dolores voluptatum blanditiis dolor.
-						Architecto, pariatur eligendi.
-					</h2>
-					<button onClick={toggleShow}>{show ? '-' : '+'}</button>
-				</header>
-
-				<p className={`text-justify italic pr-10 ${!show ? 'hidden' : null}`}>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad corrupti,
-					eius eligendi placeat fugit nulla possimus dolorum natus iusto
-					doloribus ea doloremque quidem minima quasi quae error laborum. At,
-					repudiandae!
-				</p>
-			</div>
+			<Item />
 		</div>
 	);
 };

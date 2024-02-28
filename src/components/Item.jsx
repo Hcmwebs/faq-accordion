30cfc93762fc6b1fd8260b8f0b46f63b45417800
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-28 21:50:28 +1300

    added a divider

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,6 +4,7 @@ const Item = () => {
 	return (
 		<div className='grid grid-col-1 place-items-center gap-y-4 px-6 text-justify'>
 			<Question />
+			<div className='divider divider-black'></div>
 			<Answer />
 		</div>
 	);

81e7e71d1b0761182e57db4c0afc9d77246f600c
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-28 21:10:56 +1300

    indexed the components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,9 @@
 import Answer from './Answer';
+import ButtonClose from './ButtonClose';
+import ButtonOpen from './ButtonOpen';
 import Card from './Card';
 import Item from './Item';
 import Question from './Question';
 import Title from './Title';
 
-export { Card, Title, Item, Question, Answer };
+export { ButtonClose, ButtonOpen, Card, Title, Item, Question, Answer };

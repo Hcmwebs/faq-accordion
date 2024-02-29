86b16a81552e0819fdd3a67a205cb0ceef7d9f40
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-29 22:54:41 +1300

    fixed transitions and adjusted spacing

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -5,7 +5,7 @@ const Accordion = ({ id, question, answer }) => {
 	const { activeId, toggleQuestion } = useGlobalContext();
 	const isActive = id === activeId;
 	return (
-		<div className='grid grid-col-1 py-5 lg:py-6 gap-y-6'>
+		<div className='grid grid-col-1 gap-y-5 lg:gap-y-6'>
 			<button
 				type='button'
 				className='w-full px-4 py-3 bg-transparent text-black hover:text-primary border-0 flex justify-between align-center shadow-sm cursor-pointer'
@@ -20,19 +20,16 @@ const Accordion = ({ id, question, answer }) => {
 					<ButtonOpen className='cursor-pointer' />
 				)}
 			</button>
-			{isActive && (
-				<div
-					className={`grid overflow-hidden transition-all duration-700 ease-in-out ${
-						isActive
-							? 'grid-rows-[1fr] opacity-100'
-							: 'grid-rows-[0fr] opacity-0'
-					}`}
-				>
-					<p className='overflow-hidden py-3 px-4 text-left text-neutral text-base font-normal'>
-						{answer}
-					</p>
-				</div>
-			)}
+
+			<div
+				className={`grid overflow-hidden transition-all duration-700 ease-in-out ${
+					isActive ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'
+				}`}
+			>
+				<p className='overflow-hidden px-4 py-3 text-left text-neutral text-base font-normal'>
+					{answer}
+				</p>
+			</div>
 		</div>
 	);
 };

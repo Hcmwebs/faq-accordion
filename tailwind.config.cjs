2624a7a73aeaa648aeba540b04f71d1241677b4a
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-29 14:20:09 +1300

    updated the colors of the theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,4 +10,20 @@ export default {
 		},
 	},
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: 'hsl(281, 83%, 54%)',
+					secondary: 'hsl(292, 42%, 14%)',
+					neutral: 'hsl(292, 16%, 49%)',
+					'base-100': 'hsl(275,100%, 97%)',
+					white: 'hsl(0, 0%, 100%)',
+					black: 'hsl(292, 42%, 14%)',
+				},
+			},
+			'fantasy',
+			'halloween',
+		],
+	},
 };

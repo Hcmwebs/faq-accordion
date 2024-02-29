a21f5cab0e8be95af1c4bac391d11a2d2425edbc
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-03-01 00:58:40 +1300

    adjusted border radius on large screens

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { useGlobalContext } from '../context/AppContext';
 const Card = () => {
 	const { questions } = useGlobalContext();
 	return (
-		<div className='grid grid-col-1 place-item-center p-6 lg:p-10 bg-white w-[90vw] max-w-sm lg:max-w-xl rounded-lg shadow-xl hover:shadow-2xl absolute top-[90px] lg:top-[150px] left-[50%] -translate-x-1/2'>
+		<div className='grid grid-col-1 place-item-center p-6 lg:p-10 bg-white w-[90vw] max-w-sm lg:max-w-xl rounded-lg lg:rounded-2xl shadow-xl hover:shadow-2xl absolute top-[90px] lg:top-[150px] left-[50%] -translate-x-1/2'>
 			<Title text='FAQs' />
 			{questions.map((question) => {
 				return <Accordion key={question.id} {...question} />;
